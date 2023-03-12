b93bd38a1a650d8bb2d8bda1d44da536342723c6
sooyoungkang <kang.sooyoung@hotmail.com>
2023-03-13 00:06:24 +0900

    Update network Utility. Add conversion from ip address to uint32

--- network_utils.cpp
+++ network_utils.cpp
@@ -5,6 +5,12 @@
 #include <ws2tcpip.h> // #include <sys/socket.h> - defined in IEEE Std. 1003.1 (POSIX), but Windows is non-compliant with the POSIX standard.
 #include "network_utils.h"
 
+//  Network Byte Order **: The TCP/IP standard network byte order is big-endian. In order to participate in a TCP/IP network, little-endian systems usually bear the burden of conversion to network byte order.
+
+// Network Address Format **: A standard internet address uses a two-part, 32-bit address field. The first part of the address field contains the network address; the second part contains the local address. The four different types of address fields are classified as A, B, C, or D, depending on the bit allocation. (Class D addresses are not supported in TCP/IP for z/VM.)
+// https://www.ibm.com/docs/en/zvm/7.1?topic=addressing-network-address-format
+
+// uint32_t ip_addr into A.B.C.D format.
 char *convert_ip_n_to_p(uint32_t ip_addr, char *output_buffer)
 {
 
@@ -23,9 +29,25 @@ char *convert_ip_n_to_p(uint32_t ip_addr, char *output_buffer)
     // Host TO Network translation
     // convert values between host and network byte order
     // makes sure the endian of a 32 bit data value is correct (Big endian)
-
     ip_addr = htonl(ip_addr);
+
+    // const char *inet_ntop(int af, const void *src, char *dst, socklen_t size);
+    //  converts from an Internet address in binary format, specified by src, to standard text format, and places the result in dst, if size, the space available in dst, is sufficient.
+    // https://www.ibm.com/docs/en/zvse/6.2?topic=SSB27H_6.2.0/fa2ti_call_inet_ntop.htm
     inet_ntop(AF_INET, &ip_addr, out, 16);
     out[15] = '\0';
     return out;
+}
+
+//  converts IP Address in A.B.C.D format in uint32_t format.
+uint32_t convert_ip_p_to_n(const char *ip_addr)
+{
+    uint32_t binary_prefix = 0;
+
+    // int inet_pton(int af, const char *restrict src, void *restrict dst);
+    // This function converts the character string src into a network address structure in the af address family, then copies the network address structure to dst. The af argument must be either AF_INET or AF_INET6. dst is written in network byte order. (big endian)
+
+    inet_pton(AF_INET, ip_addr, &binary_prefix);
+    binary_prefix = htonl(binary_prefix);
+    return binary_prefix;
 }
